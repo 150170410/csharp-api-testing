73ad6906c9ba46e02c4f12859f271f357a987240
Margarita Lukjanska <margarita.lukjanska@gmail.com>
2019-06-22 20:34:11 +0100

    Initial commit - single test case example
    for Google Books API Volume endpoint https://developers.google.com/books/docs/v1/reference/volumes/get

---
+++ APITests/TestFixtureBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace APITests
+{
+    public abstract class TestFixtureBase
+    {
+        /// <summary>
+        /// Add here clean up tasks that you want to run After the testfixture has been run.
+        /// </summary>
+        protected List<Task> CleanupTasks { get; } = new List<Task>();
+
+        /// <summary>
+        /// Because the OneTimeTearDown will be inherited by the derived class, it will
+        /// run after each fixture that derives this base class.
+        /// See https://github.com/nunit/docs/wiki/OneTimeTearDown-Attribute for more
+        /// </summary>
+        [OneTimeTearDown]
+        public async Task OneTimeTearDownTasks()
+        {
+            CleanupTasks.ForEach(t => t.Start());
+            await Task.WhenAll(CleanupTasks);
+        }
+    }
+}
